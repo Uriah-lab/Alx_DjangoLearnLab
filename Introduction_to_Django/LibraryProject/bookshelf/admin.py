d41ff8aad91ba21d77da5e24ca3398a5ca0ecaf5
Uriah Kivumbi <kivumbiuriah@gmail.com>
2025-02-24 15:31:14 +0300

    Customized admin interface for Book model

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Display fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add filters to the admin panel
+    list_filter = ('author', 'publication_year')
+    
+    # Add search capabilities for the title and author fields
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom BookAdmin class
+admin.site.register(Book, BookAdmin)
