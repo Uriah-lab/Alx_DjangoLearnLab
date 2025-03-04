08ba43dceaedb103b78481ae941e36cd176abf97
Uriah Kivumbi <kivumbiuriah@gmail.com>
2025-03-04 16:15:48 +0300

    Added token authentication and permission classes to API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookViewSet(viewsets.ModelViewSet):
     """
@@ -10,3 +11,10 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+# BookViewSet is restricted to authenticated users, only those with valid tokens can access this API.
+# To obtain a token, use the /api-token-auth/ endpoint by passing your username and password.
+# After authentication, the token must be included in the Authorization header as:
+# Authorization: Token <your_token>
