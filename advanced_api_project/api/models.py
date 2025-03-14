6f4ff549bfc048ca13e7fe34066cdeae9e84bbed
MAC <mac@MACs-MacBook-Pro.local>
2025-03-14 14:16:25 +0300

    Completed work on Django REST API

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """Model to store author information"""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    """Model to store book details linked to an Author"""
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
