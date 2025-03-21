6bb176509a0197ee64d3ad593f62feba05b31edc
MAC <mac@MACs-MacBook-Pro.local>
2025-03-21 18:51:12 +0300

    Implemented user authentication with login, logout, and profile views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import UpdateProfileForm
 
-# Create your views here.
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UpdateProfileForm(instance=request.user)
+    
+    return render(request, 'blog/profile.html', {'form': form})
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -120,4 +120,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
